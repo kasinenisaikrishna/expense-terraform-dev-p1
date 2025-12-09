3878d99e892b52c5dd6bf6258f1465a41ac3a5f9
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-09 18:43:48 +0530

    terraform

--- 40-apps/expense.sh
+++ 40-apps/expense.sh
@@ -1,8 +1,28 @@
+# #!/bin/bash
+# dnf install ansible -y
+# cd /tmp
+# git clone https://github.com/kasinenisaikrishna/expense-ansible2.git
+# cd expense-ansible2
+# ansible playbook -i inventory.ini mysql.yaml
+# ansible playbook -i inventory.ini backend.yaml
+# ansible playbook -i inventory.ini frontend.yaml
+
 #!/bin/bash
-dnf install ansible -y
+
+# Wait for network and DNF to be ready
+sleep 20
+
+# Install needed packages
+dnf install -y git ansible
+
 cd /tmp
+
+# Clone repository
 git clone https://github.com/kasinenisaikrishna/expense-ansible2.git
+
 cd expense-ansible2
-ansible playbook -i inventory.ini mysql.yaml
-ansible playbook -i inventory.ini backend.yaml
-ansible playbook -i inventory.ini frontend.yaml
+
+# Run Ansible playbooks
+ansible-playbook -i inventory.ini mysql.yaml
+ansible-playbook -i inventory.ini backend.yaml
+ansible-playbook -i inventory.ini frontend.yaml
