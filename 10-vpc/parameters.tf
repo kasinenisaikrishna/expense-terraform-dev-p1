ca137f6f92e8aa7c9b6d8319a6d8c447e7a3b081
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-09 11:33:48 +0530

    terraform

--- 10-vpc/parameters.tf
+++ 10-vpc/parameters.tf
@@ -7,5 +7,5 @@ resource "aws_ssm_parameter" "vpc_id" {
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
   type  = "StringList"
-  value = module.vpc.public_subnet_ids
+  value = join(",", module.vpc.public_subnet_ids) # converting list to stringlist as aws needs the values in the form of stringlist
 }
